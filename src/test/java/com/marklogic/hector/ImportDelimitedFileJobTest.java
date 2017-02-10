028cd5df45babf6fe914103f2cd5fab4686c1557
Scott Stafford <sstafford@marklogic.com>
2017-02-10 15:00:39 -0500

    Add support for document_type

--- src/test/java/com/marklogic/hector/ImportDelimitedFileJobTest.java
+++ src/test/java/com/marklogic/hector/ImportDelimitedFileJobTest.java
@@ -43,6 +43,19 @@ public class ImportDelimitedFileJobTest extends AbstractJobRunnerTest {
     @Test
     public void ingestDelimitedBabyNamesWithUriTransformTest() throws Exception {
         JobExecution jobExecution = getJobLauncherTestUtils().launchJob(getJobParametersWithUriTransform());
+        getClientTestHelper().assertCollectionSize("Expecting 4882 files in baby-name collection", "baby-name", 4882);
+        getClientTestHelper().parseUri("2011-ALEC.xml", "baby-name");
+    }
+
+    @Test
+    public void ingestBabyNamesToJsonTest() throws Exception {
+        JobParametersBuilder jpb = new JobParametersBuilder();
+        jpb.addString("input_file_path", ".\\src\\test\\resources\\Most_Popular_Baby_Names_NYC.csv");
+        jpb.addString("delimited_root_name", "baby-name");
+        jpb.addString("document_type", "xml");
+        jpb.addString("output_collections", "baby-name");
+        jpb.addString("document_type", "json");
+        JobExecution jobExecution = getJobLauncherTestUtils().launchJob(jpb.toJobParameters());
         getClientTestHelper().assertCollectionSize("Expecting 13962 files in baby-name collection", "baby-name", 13962);
     }
 
