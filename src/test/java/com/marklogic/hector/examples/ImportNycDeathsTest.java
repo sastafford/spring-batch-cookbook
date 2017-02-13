e5737c12494cb987ce1a2ba9585151fd2746d7e7
Scott Stafford <sstafford@marklogic.com>
2017-02-13 11:14:49 -0500

    Add failing example tests

---
+++ src/test/java/com/marklogic/hector/examples/ImportNycDeathsTest.java
@@ -0,0 +1,27 @@
+package com.marklogic.hector.examples;
+
+import com.marklogic.hector.ImportDelimitedFileJob;
+import com.marklogic.spring.batch.test.AbstractJobRunnerTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(classes = { ImportDelimitedFileJob.class })
+public class ImportNycDeathsTest extends AbstractJobRunnerTest {
+
+    private JobParametersBuilder jpb = new JobParametersBuilder();
+
+    @Before
+    public void initJobParameters() {
+        jpb.addString("input_file_path", ".\\src\\test\\resources\\nyc-deaths.csv");
+        jpb.addString("output_collections", "nyc");
+    }
+
+    @Test
+    public void ingestDelimitedBabyNamesDefaultThreadAndChunkSizeTest() throws Exception {
+        JobExecution jobExecution = getJobLauncherTestUtils().launchJob(jpb.toJobParameters());
+        getClientTestHelper().assertCollectionSize("Expecting 199 files in baby-name collection", "baby-name", 199);
+    }
+}
